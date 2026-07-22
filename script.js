1f600bad8846d90a074a938b6e0316aac8e2ed1e
smartvaseem7-jpg <smartvaseem7@gmail.com>
2026-07-22 17:27:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+console.log("WM POINT Loaded Successfully!");
+
+const btn = document.querySelector(".btn");
+
+if(btn){
+btn.addEventListener("click", function(e){
+e.preventDefault();
 alert("Welcome to WM POINT 🏏");
+});
+}
+
+});
